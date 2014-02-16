e0510db8dc7ebfbecb064bf0c1499fac5b712da2
Johan Forsberg <johan@slentrian.org>
2014-02-16 17:46:13 +0100

    basic pathfinding

--- js/world.js
+++ js/world.js
@@ -3,17 +3,82 @@ var world;
 (function () {
 
     // Representation of the game world
-    function World() { 
+    function World(spec) {
+        console.log("spec", spec);
+        // actions
         this.toggle = toggle;
         this.console =  function (s) {console.log("World console: " + s);};
 
-        this.doors = {
-            1 : {open: false},
-            2 : {open: false}
-        };
+        // data
+        this.doors = spec.doors;
+        this.rooms = spec.rooms;
+    }
+
+    World.prototype.getConnectedRooms = function (room_number) {
+        var door, connected = {};
+        for (var d in this.doors) {
+            door = this.doors[d];
+            if (door.open && door.rooms && door.rooms.indexOf(room_number) > -1) {
+                door.rooms.forEach(function (r) {
+                    if (r != room_number)
+                        connected[r] = true;
+                });
+            }
+        }
+        return Object.keys(connected);
     };
 
-    world = new World();
+    World.prototype.getShortestPath = function (room1, room2) {
+        var step = 0, queue = {}, i = 0, pos, path, nearest, connected, dist, old, totdist;
+        queue[room2] = 0;
+        // Note: this implementation depends on objects keeping keys in the order they
+        // were added. This seems to not be true for numeric strings in FF. Watch out!
+        while (i < _.keys(queue).length) {
+            pos = _.keys(queue)[i];
+            step = _.values(queue)[i];
+            if (pos == room1) {
+                path = [];
+                nearest = queue[room1];
+                while (pos != room2) {
+                    connected = this.getConnectedRooms(pos);
+                    connected.forEach(function (room) {
+                        dist = queue[room];
+                        if (room == room2 || (dist && dist < nearest)) {
+                            nearest = dist;
+                            pos = room;
+                        }
+                    });
+                    path.push(pos);
+                }
+                return path;
+            }
+            connected = this.getConnectedRooms(pos);
+            connected.forEach(function (room) {
+                old = queue[room];
+                totdist = step + 1;
+                if (!old || old > totdist)
+                    queue[room] = totdist;
+            });
+            i++;
+        }
+        return false;
+    };
+
+    world = new World({
+        doors: {
+            "1": {open: false},
+            "2": {open: true, rooms: ["a", "b"]},
+            "3": {open: true, rooms: ["a", "c"]}
+        },
+        rooms: {
+            "a": {},
+            "b": {},
+            "c": {}
+        }
+    });
+
+    console.log("neigh", world.getConnectedRooms("b"));
+    console.log("path", world.getShortestPath("b", "c"));
 
     // Toggle status of something, depending on its type
     function toggle (evt) {
