475e94a360965fc2dd027c39a9f5ab5c5dbd2fe8
Jonas Ransjö <ransjo@gmail.com>
2014-02-20 16:31:27 +0100

    transgressed immutability of vector instances

--- js/vector.js
+++ js/vector.js
@@ -1,28 +1,50 @@
 function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.init = function (x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  this.set = function (v) {
+    this.x = v.x;
+    this.y = v.y;
+  }
+  
+  this.increase = function (v) {
+    this.x += v.x;
+    this.y += v.y;
+  }
+
+  this.decrease = function (v) {
+    this.x -= v.x;
+    this.y -= v.y;
+  }
+
 }
 
 Vector.prototype.copy = function () {
-    return new Vector(this.x, this.y);
+  return new Vector(this.x, this.y);
 };
 
 Vector.prototype.add = function (v) {
-    return new Vector(this.x + v.x, this.y + v.y);
+  return new Vector(this.x + v.x, this.y + v.y);
 };
 
 Vector.prototype.subtract = function (v) {
-    return new Vector(this.x - v.x, this.y - v.y);
+  return new Vector(this.x - v.x, this.y - v.y);
 };
 
 Vector.prototype.multiply = function (a) {
-    return new Vector(this.x * a, this.y * a);
+  return new Vector(this.x * a, this.y * a);
 };
 
 Vector.prototype.length = function () {
-    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  return Math.sqrt(x*x + y*y);
 };
 
 Vector.prototype.dot = function (v) {
-    return this.x * v.x + this.y * v.y;
+  return this.x * v.x + this.y * v.y;
 };
+
